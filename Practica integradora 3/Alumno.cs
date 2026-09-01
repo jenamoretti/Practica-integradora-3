d6b604390f790b7e476a7a91059893546908f2de
jenamoretti <jenaromoretti2005@gmail.com>
2026-09-01 17:37:22 -0300

    Primer commit

---
+++ Practica integradora 3/Alumno.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Practica_integradora_3;
+
+namespace Practica_integradora_3
+{
+    public class Alumno
+    {
+        public string nombre;
+        public int legajo;
+        public double nota1;
+        public double nota2;
+
+        public Alumno(string nombre, int legajo, double nota1, double nota2)
+        {
+            this.nombre = nombre;
+            this.legajo = legajo;
+            this.nota1 = nota1;
+            this.nota2 = nota2;
+        }
+    }
+}
---
+++ Practica integradora 3/Program.cs
@@ -0,0 +1,9 @@
+﻿using Practica_integradora_3;
+
+Alumno alumno1 = new Alumno("Juan", 12345, 8.5, 7.0);
+Alumno alumno2 = new Alumno("María", 67890, 9.0, 8.5);
+
+alumno1.nombre = "Facundo";
+
+Console.WriteLine("Nombre: " + alumno1.nombre + "- Legajo: " + alumno1.legajo);
+Console.WriteLine("Nombre: " + alumno2.nombre + "- Legajo: " + alumno2.legajo);
